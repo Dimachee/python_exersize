634be38f3e43f669c4dd6048be34ef0edbb3320d
dimachu <dima.che96@gmail.com>
2023-01-20 10:52:49 +0200

    finish

--- exersize_2.py
+++ exersize_2.py
@@ -1,6 +1,5 @@
-def new ( user_len,user_element):
-    return user_len == 8 , user.count(user_element) ==3 
-user  = [19, 19, 15, 7, 5, 5, 1, 2]
-user_len = len(user)
-user_element  = user[5]
-print(new(user_len, user_element))
+def new (user):
+    return user.count(5) == 3 
+user = [19, 19, 15, 5, 5, 5, 1, 2]
+lenghth = len(user)
+print(lenghth, new(user))
