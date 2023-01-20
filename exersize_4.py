bc31b53c90d4bbe0c27da99e6b0387aab4fc220c
dimachu <dima.che96@gmail.com>
2023-01-20 11:50:21 +0200

    2 options

--- exersize_4.py
+++ exersize_4.py
@@ -1,8 +1,11 @@
-def new(user):
-    if (user / 2) ==0:   
-        print(user, next_pile)
-    else:
-        print(user, next_pile)   
-user = int(input('Enter even or odd number: '))
-next_pile = user + 2 
-new(user)
+def new(n):
+    return [n * i + 2   for i  in  range(n)]
+n = 2 
+print(new(n))
+
+
+x = range(2)
+t = 2
+for i in x:
+    v = [t *i]
+    print(v)
