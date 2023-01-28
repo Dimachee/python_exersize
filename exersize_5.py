2697cd5d1bd1fec81dc685dd5d663eeaaeb48afc
dimachu <dima.che96@gmail.com>
2023-01-28 19:57:41 +0200

    Update exersize_5.py

--- exersize_5.py
+++ exersize_5.py
@@ -1,4 +1,4 @@
 def  new(n):
     return   n[len(n)- 2] in n[len(n) -1]  and n[len(n)- 2] != n[len(n)- 1] 
-n = ["asd", "dsada", "fd", "fdddddd"]
+n = ["asd", "dsada", "fd", "fddddddd"]
 print(new(n))
